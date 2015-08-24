9122dacb09e57f880d5025a62608a295c0284103
peyman <peyman@maindevelopment.ca>
2015-08-23 22:17:59 -0700

    First Commit

---
+++ version.h
@@ -0,0 +1,7 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+#define VERSION_DATE "Sun, 23 Aug 2015 21:14:22 -0700"
+#define VERSION_HASH "a21c4fd"
+#define VERSION_BRANCH "master"
+#define VERSION_REPO "UNKNOWN"
+#endif
